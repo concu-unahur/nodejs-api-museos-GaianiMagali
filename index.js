194d7439380268cf00bf9ded6c40f48559052f23
GaianiMagali <magaliantonella.gaiani@gmail.com>
2020-03-13 13:49:57 -0300

    completo

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const superagent = require('superagent');
+const fs = require('fs');
+
+// function imprimirMuseos(error, respuesta) {
+//   if (error) {
+//     throw new Error('algo se rompió', error);
+//   }
+
+//   const cantidad = respuesta.body.count;
+//   const museos = respuesta.body.results;
+
+//   console.log(`Se encontraron ${cantidad} museos.`);
+//   console.log(`El primer museo se llama ${museos[0].nombre}.`)
+// }
 
 function imprimirMuseos(error, respuesta) {
-  if (error) {
-    throw new Error('algo se rompió', error);
-  }
-
-  const cantidad = respuesta.body.count;
-  const museos = respuesta.body.results;
-
-  console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
+    if (error) {
+      throw new Error('algo se rompió', error);
+    }
+  
+    const cantidad = respuesta.body.count;
+    const museos = respuesta.body.results;
+  
+  const museums = museos.map(m => {
+    return `${m.nombre} (${m.direccion}). Por cualquier consulta comunicarse al ${m.telefono}`
+  })
+  
+  fs.writeFile('museos.txt', museums, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
 }
 
 console.log('Antes de llamar a superagent')
